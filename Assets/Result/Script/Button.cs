4486be2dc3932cc5af1066cebff843b5cebae421
YokoyamaYuta <96154391+YokoyamaYuta@users.noreply.github.com>
2022-05-07 18:04:07 +0900

    リザルト画面完成

---
+++ Assets/Result/Script/Button.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public void ButtonC()
+    {
+        SceneManager.LoadScene("SampleScene");   //sceneåƒÇ—èoÇµ
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
