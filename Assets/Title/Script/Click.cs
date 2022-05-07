b6af0ff3a8bfcf26258d1d2e2df22fbd02d4d3ca
204-21\student <kasyuru6521@gmail.com>
2022-05-07 18:52:16 +0900

    Title画面完成

---
+++ Assets/Title/Script/Click.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Click : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        
+    }
+    void Update()
+    {
+        //クリックしたら
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Result screen(Ranger)");
+            Debug.Log("ki");
+        }
+        //スペース押したら
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Result screen(Ranger)");
+            Debug.Log("ki");
+        }
+
+    }
+
+}
